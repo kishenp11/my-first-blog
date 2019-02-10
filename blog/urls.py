4374f8fdb27e8ec52161652c198e06f088e5aabd
Kishen Pujara <kishenp11@gmail.com>
2019-02-09 23:03:04 -0500

    Changed the HTML for the site.

---
+++ blog/urls.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  9 21:24:44 2019
+
+@author: kishenpujara
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+        path('', views.post_list, name='post_list'),
+]
